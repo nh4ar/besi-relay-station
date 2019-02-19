ad2b8d3b014fad52586e62a0257186fc437f9413
Nutta Homdee <nh4ar@virginia.edu>
2019-02-19 14:38:52 -0500

    Update initializer.sh

--- initializer.sh
+++ initializer.sh
@@ -5,8 +5,6 @@ sudo apt-get install build-essential python-dev python-setuptools python-pip pyt
 sudo apt-get install python-lightblue bluez python-gobject python-dbus bluez-utils python-bluez -y
 sudo apt-get install dos2unix -y
 sudo pip install Adafruit_BBIO 
-cd /opt/scripts/tools/
-./update_kernel.sh
 sudo pip install setuptools --upgrade
 sudo pip uninstall distribute -y
 sudo pip install --upgrade setuptools
@@ -14,4 +12,7 @@ sudo pip --no-cache-dir install --upgrade numpy
 sudo apt-get install python-matplotlib python-scipy python-sklearn python-simplejson python-eyed3 -y
 sudo pip --no-cache-dir install --upgrade hmmlearn pydub eyed3
 sudo pip --no-cache-dir install https://pypi.python.org/packages/e4/92/c3d26ceeef8f9f960224a8286114988db2cec6f342ad98a0031285ec364d/pyAudioAnalysis-0.1.3.tar.gz
+cd /opt/scripts/tools/
+git pull
+sudo ./update_kernel.sh
 reboot
