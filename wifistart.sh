b018315ed86d957474cb6af401c9dcfc6603d7ce
Nutta Homdee <nh4ar@virginia.edu>
2019-02-24 21:59:43 -0500

    Update wifistart.sh

--- wifistart.sh
+++ wifistart.sh
@@ -7,6 +7,3 @@ connmanctl disable wifi
 connmanctl enable wifi
 connmanctl scan wifi
 connmanctl services
-connmanctl agent on
-connmanctl connect wifi_e0b94d199709_424553495f524f55544552_managed_none
-reboot
