71093a566aee6c0847f3b6c0f1c88cf3429f292a
Nutta Homdee <nh4ar@virginia.edu>
2019-04-03 12:53:33 -0400

    Update wifi-reset.sh

--- wifi-services/wifi-reset/wifi-reset.sh
+++ wifi-services/wifi-reset/wifi-reset.sh
@@ -3,9 +3,10 @@
 # Identify wifi interfaces as rows from standard output of iwconfig (NOT standard
 # error, those are non-wifi interfaces) which start without whitespace.
 sleep 30
-iwconfig 2> /dev/null | grep -o '^[[:alnum:]]\+' | while read x; do ifdown $x; done
 connmanctl disable wifi
+iwconfig 2> /dev/null | grep -o '^[[:alnum:]]\+' | while read x; do ifdown $x; done
 # Bring all wifi interfaces up.
 sleep 30
-iwconfig 2> /dev/null | grep -o '^[[:alnum:]]\+' | while read x; do ifup $x; done
 connmanctl enable wifi
+iwconfig 2> /dev/null | grep -o '^[[:alnum:]]\+' | while read x; do ifup $x; done
+
